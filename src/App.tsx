38274742fe651596bc99381b347ce9894c758552
AbdallahShnaino <abdallah.shnaino@gmail.com>
2025-02-14 14:03:25 +0200

    SCRUM-28 setup routing and guard to enable user restrictions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import InvoiceListScreen from "./screens/invoiceList/InvoiceListScreen";
-// import ProductPage from "./screens/ProductPage/ProductPage";
+import { Role } from "./@types";
+import ClientScreen from "./screens/client.screen";
+import Guarded from "./components/common/guarded-route/guarded-route.component";
+import NotFound from "./screens/not-found.screen";
+import HomeScreen from "./screens/home.screen";
 
 function App() {
-  return <InvoiceListScreen/>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route
+          path="/client"
+          element={
+            <Guarded roles={[Role.CLIENT]}>
+              <ClientScreen />
+            </Guarded>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
