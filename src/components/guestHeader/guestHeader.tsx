a1a4e7f60a208c78952a99f605b9d744bce5d270
AmjadShabaneh <129591213+AmjadShabaneh@users.noreply.github.com>
2025-02-16 18:49:38 +0200

    .

---
+++ src/components/guestHeader/guestHeader.tsx
@@ -0,0 +1,11 @@
+
+
+const GuestHeader = () => {
+  return (
+    <header className="guest-header">
+
+    </header>
+  )
+}
+
+export default GuestHeader
---
+++ src/screens/ProductsGuest/products.tsx
@@ -0,0 +1,14 @@
+import GuestHeader from "../../components/guestHeader/guestHeader"
+
+const Products = () => {
+  return (
+    <div className="products-screen">
+       <GuestHeader>
+    
+  </GuestHeader> 
+    </div>
+  
+  )
+}
+
+export default Products
